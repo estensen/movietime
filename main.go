9b6eb91bc5bbf24cd6998f0898d8c798f1b8a60d
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-06-16 22:49:20 +0200

    Get movie name from CLI

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
+	app := &cli.App{
+		Name: "reviews",
+		Usage: "get aggregated reviews of a movie",
+		Action: func(c *cli.Context) error {
+			movie := c.Args().Get(0)
+			fmt.Println(movie)
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(1)
+
+
 	resp, err := http.Get("https://nrk.no")
 	if err != nil {
 		log.Fatal(err)
